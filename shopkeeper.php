bf5d357492f806e4da8e1b72d1bbf336cbec2361
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-16 23:10:17 +0530

    shopkeeper

---
+++ shopkeeper.php
@@ -0,0 +1,19 @@
+<?php
+   include 'shopkeeper_sidebar.php';
+?>  
+<div class="content">
+<center>
+  <br><br><div><img src="images/logo.jpeg" alt="logo" class="center" width=200 height=300/></div>
+  </center>
+  <br><br><br>
+  <div class="center">
+  <form class="example" action="/action_page.php">
+  <input type="text" placeholder="Search Product" name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+</div>
+<br><br>
+<div class="footer">
+			<p>© Tactota Solutions All rights reserved </p>
+		</div>
+</div>
