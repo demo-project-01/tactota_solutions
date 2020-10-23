77337f4e89a20a21a17163d06dee0e43b789cb23
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-23 23:56:55 +0530

    clerk_dashboard

--- clerk_sidebar.php
+++ clerk_sidebar.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Clerk Dashboard</title>
-<link rel="stylesheet" href="style1.css">
-<link rel="stylesheet" href="css/signup.css">
-  <link rel="stylesheet" href="css/view_user.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-<script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Clark Dashboard</title>
+  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="css/view_user.css"-->
+  <!--link rel="stylesheet" href="css/signup.css"-->
+  <link rel="stylesheet" href="clerk.css">
+  <!--link href="css/signup.css" rel="stylesheet" type="text/css"/-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+  <script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
 
 </script>
 
