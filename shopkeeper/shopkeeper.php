2f7598c257b168cd3217a86066987eec15cfc759
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-25 23:18:17 +0530

    Create shopkeeper_interface

---
+++ shopkeeper/shopkeeper.php
@@ -0,0 +1,26 @@
+<?php
+   include 'shopkeeper_sidebar.php';
+?>  
+<div class="content">
+
+  <br><br><div class="image"><img src="images/logo.jpeg" alt="logo" class="centers" width=200 height=300/></div>
+  
+  <br><br>
+  <div class="center">
+    
+  <form class="example" action="/action_page.php">
+  <input type="text" placeholder="Search Product" name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+</div>
+
+</div>
+<div class="footer">
+			<p>© Tactota Solutions All rights reserved </p>
+</div>
+
+
+
+    
+
