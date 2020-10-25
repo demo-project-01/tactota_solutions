03386dc9a61b8c42f685e3028d4066bf782c189b
nuwansasanka <56679037+nuwansasanka@users.noreply.github.com>
2020-10-26 00:14:24 +0530

    nuwan

---
+++ dash.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Clerk Dashboard</title>
+<link rel="stylesheet" href="style1.css">
+<link rel="stylesheet" href="css/signup.css">
+  <link rel="stylesheet" href="css/view_user.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+<script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
+
+</script>
+
+</script>
+</head>
+<body>
+
+<input type="checkbox" id="check">
+<!--header area start-->
+<header>
+  <label for="check">
+    <i class="fas fa-bars" id="sidebar_btn"></i>
+  </label>
+  <div class="left_area">
+    <h3>TACTOTA<span> SOLUTION</span></h3>
+  </div>
+  <ul class="profile-wrapper">
+          <li>
+
+               <div class="profile">
+                  
+                  <i class="far fa-user-circle fa-2x"></i>
+                     <ul class="menu">
+                           <li><a href="setting_edit.php">setting</a></li>
+                           <li><a href="logout.php">Log out</a></li>
+                     </ul>    
+               </div>
+          </li>
+            
+   </ul> 
+</header>
+<!--header area end-->
+
+<div class="sidebar">
+
+  <a href="#"><i class="fas fa-home"></i></i><span>Dashboard</span></a>
+  
+</div>
+<!--sidebar end-->
+     
