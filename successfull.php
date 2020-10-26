3d6327833e391e86fee9967516f79d9ff338fef2
nuwansasanka <56679037+nuwansasanka@users.noreply.github.com>
2020-10-27 01:56:56 +0530

    Add files via upload

---
+++ successfull.php
@@ -0,0 +1,31 @@
+<?php
+include 'dash.php';
+
+?>
+<link rel="stylesheet" href="css/signup.css">
+  <link rel="stylesheet" href="css/view_user.css">
+</head>
+<body>
+	<div>
+		<h1>Successful Register</h1>
+        <br/>
+        
+		
+		<div class="main-box">
+			
+            <div class="sub-container">
+				<form action="#" method="post">
+                   
+               <b><h7>Payment Successful</h7></b>	
+                    
+				</form>
+				<br/>
+				<input type="submit" value="Home">
+                
+			</div>
+			
+		</div>
+		
+	
+	</div>
+</body>
