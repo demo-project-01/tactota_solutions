84ee9e5b0f718968c890467a896cde032199bf2b
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-27 01:19:02 +0530

    clerk.php

---
+++ clerk.php
@@ -0,0 +1,67 @@
+<?php
+   include 'clerk_sidebar.php';
+?>
+
+<div class="content">
+    <div class= "dash1">
+        <div class= "dash2">
+        </div>
+    </div>
+    <div class= "dash3">
+        <div class= "dash4">
+        </div>
+    </div>
+    
+    <div class="wrapper">
+        <div class="list_wrap">
+            <div class="subcontent">
+        <h1><p>Stock Reminders</p></h1>
+        </div>
+            <ul>
+                <li class="github">
+                    <div class="list">
+                        <div class="contentc">
+                            <b>item1</b>
+                        </div>
+                    </div>
+                </li>
+                <li class="codepen">
+                    <div class="list">
+                        <div class="contentc">
+                            <b>item2</b>
+                        </div>
+                    </div>
+                </li>
+                <li class="dribble">
+                    <div class="list">
+                        <div class="contentc">
+                            <b>item3</b>
+                        </div>
+                    </div>
+                </li>
+                <li class="reddit">
+                    <div class="list">
+                        <div class="contentc">
+                            <b>item4</b>
+                        </div> 
+                    </div>
+                </li>
+                <li class="quora">
+                    <div class="list">
+                        <div class="contentc">
+                            <b>item5</b>
+                        </div> 
+                    </div>
+                </li>
+                <div class="view">
+                <b><a href="login">View All Products</a></b>
+                </div>
+            </ul>
+        </div>
+    </div> 
+  
+</div>
+<div class="footer">
+			<p>© Tactota Solutions All rights reserved </p>
+</div>
+  
