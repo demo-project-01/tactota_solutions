7f5057a59d7de71b6cb628b0cb0d47cba13286e3
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-27 17:18:42 +0530

    new update clerk_sidebar.php

--- clerk_sidebar.php
+++ clerk_sidebar.php
@@ -5,11 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Clark Dashboard</title>
   <link rel="stylesheet" href="style1.css">
-  <link href="css/signup.css" rel="stylesheet" type="text/css"/>
-  <!--link rel="stylesheet" href="css/view_user.css"-->
-  <!--link rel="stylesheet" href="css/signup.css"-->
-  <link rel="stylesheet" href="clerk.css">
   <!--link href="css/signup.css" rel="stylesheet" type="text/css"/-->
+  <!--link rel="stylesheet" href="css/view_user.css"-->
+  <link rel="stylesheet" href="clerk.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
   <script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
@@ -62,6 +60,9 @@
 </div>
 <!--sidebar end-->
      
+
+
+
 
 
 
