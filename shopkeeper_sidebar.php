b95fbacc30469a170efb580f33d2d16a9061c31a
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-10-27 17:53:18 +0530

    new update shopkeeper_sidebar.php

--- shopkeeper_sidebar.php
+++ shopkeeper_sidebar.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Shopkeeper Dashboard</title>
   <link rel="stylesheet" href="style1.css">
-  <link href="css/signup.css" rel="stylesheet" type="text/css"/>
+  <!--link href="css/signup.css" rel="stylesheet" type="text/css"/-->
   <!--link rel="stylesheet" href="css/view_user.css"-->
   <link rel="stylesheet" href="shopkeeper.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
