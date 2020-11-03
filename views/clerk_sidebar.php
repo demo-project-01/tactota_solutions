60976e215ecf3daa777c542071e2dcfc090127a0
Michelle Fernando <54815746+MichelleFdo@users.noreply.github.com>
2020-11-03 11:44:33 +0530

    Update clerk_sidebar.php

--- views/clerk_sidebar.php
+++ views/clerk_sidebar.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Clark Dashboard</title>
-  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="../public/css/style1.css">
   <!--link href="css/signup.css" rel="stylesheet" type="text/css"/-->
   <link rel="stylesheet" href="../public/css/view_user.css">
   <link rel="stylesheet" href="../public/css/clerk.css">
