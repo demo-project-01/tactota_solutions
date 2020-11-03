f0420b44663cfbc34b34fcb1505209b342f965db
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-11-03 11:32:57 +0530

    Update admin_sidebar.php

--- views/admin_sidebar.php
+++ views/admin_sidebar.php
@@ -1,59 +1,53 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Dashboard</title>
-  <link rel="stylesheet" href="style1.css">
-  <!--link href="css/signup.css" rel="stylesheet" type="text/css"/-->
-  <link rel="stylesheet" href="css/view_user.css">
-  <link rel="stylesheet" href="admin.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-  <script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tactota solution</title>
+    <link rel="stylesheet" href="../public/css/style1.css">
+    <link rel="stylesheet" href="../public/css/view_user.css">
+    <link rel="stylesheet" href="../public/css/admin.css">
 
-  </script>
+    <script> src="../public/js/include.js"</script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+    <script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
 
-  </script>
 </head>
 <body>
-
-  <input type="checkbox" id="check">
-  <!--header area start-->
-  <header>
+<input type="checkbox" id="check">
+<!--header area start-->
+<header>
     <label for="check">
-      <i class="fas fa-bars" id="sidebar_btn"></i>
+        <i class="fas fa-bars" id="sidebar_btn"></i>
     </label>
     <div class="left_area">
-      <h3>TACTOTA<span> SOLUTION</span></h3>
+        <h3>TACTOTA<span> SOLUTION</span></h3>
     </div>
     <ul class="profile-wrapper">
-            <li>
+        <li>
 
-                 <div class="profile">
-                    
-                    <i class="far fa-user-circle fa-2x"></i>
-                       <ul class="menu">
-                             <li><a href="#">Edit</a></li>
-                             <li><a href="logout.php">Log out</a></li>
-                       </ul>    
-                 </div>
-            </li>
-              
-     </ul> 
-  </header>
-  <!--header area end-->
+            <div class="profile">
 
-  <div class="sidebar">
+                <i class="far fa-user-circle fa-2x"></i>
+                <ul class="menu">
+                    <li><a href="profile.php"><i class="fas fa-user-alt"></i>Profile</a></li>
+                    <li><a href="../controller/authenitication.php?action=logout"><i class="fas fa-sign-out-alt"></i>Log out</a></li>
+                </ul>
+            </div>
+        </li>
 
+    </ul>
+</header>
+<!--header area end-->
+<div class="sidebar">
     <a href="#"><i class="fas fa-home"></i></i><span>Dashboard</span></a>
     <a href="#"><i class="fas fa-cart-plus"></i></i><span>Income</span></a>
     <a href="#"><i class="fas fa-thumbs-up"></i>Review</span></a>
-    <a href="#"><i class="fas fa-users"></i></i><span>Users</span></a>
+    <a href="../controller/authenitication.php?action=user_details"><i class="fas fa-users"></i></i><span>Users</span></a>
     <a href="#"><i class="fas fa-shopping-cart"></i></i><span>Sold Item</span></a>
     <a href="#"><i class="fas fa-store"></i></i><span>Stock Details</span></a>
-    
-  </div>
-  <!--sidebar end-->
-<!-- start write other need inside content class -->
+
+</div>
+
 
