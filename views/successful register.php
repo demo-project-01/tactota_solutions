e0a301c29cb3ebd6a68d6679f068883b9b97d7b0
Michelle Fernando <54815746+MichelleFdo@users.noreply.github.com>
2020-11-03 09:20:26 +0530

    path,mvc

--- views/successful register.php
+++ views/successful register.php
@@ -4,7 +4,7 @@
 <title>Tactota Solutions Registration Form</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="css/signup.css" rel="stylesheet" type="text/css"/>
+<link href="../public/css/signup.css" rel="stylesheet" type="text/css"/>
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
