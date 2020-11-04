8791bb45bb7538045b1affa353298140a552eeaf
nuwansasanka <56679037+nuwansasanka@users.noreply.github.com>
2020-11-04 12:19:18 +0530

    changed code

--- successfull.php
+++ successfull.php
@@ -1,50 +1,19 @@
-<<<<<<< Updated upstream
-<?php
-include 'dash.php';
-
-?>
-<link rel="stylesheet" href="css/signup.css">
-  <link rel="stylesheet" href="css/view_user.css">
-</head>
-<body>
-	<div>
-		<h1>Successful Register</h1>
-        <br/>
-        
-		
-		<div class="main-box">
-			
-            <div class="sub-container">
-				<form action="#" method="post">
-                   
-               <b><h7>Payment Successful</h7></b>	
-                    
-				</form>
-				<br/>
-				<input type="submit" value="Home">
-                
-			</div>
-			
-		</div>
-		
-	
-	</div>
-=======
 <?php
 include 'header.php';
 
 ?>
 <link rel="stylesheet" href="style1.css">
+<div class="content">
+
 </head>
 <body>
 	<div>
-		<h1>Successful Register</h1>
-        <br/>
-        
 		
+</br></br></br>
 		<div class="main-box">
 			
             <div class="sub-box">
+			</br></br></br>
 				<form action="#" method="post">
                    
                <b><h2>Payment Successful</h2></b>	
@@ -57,7 +26,6 @@ include 'header.php';
 			
 		</div>
 		
-	
 	</div>
->>>>>>> Stashed changes
+
 </body>
