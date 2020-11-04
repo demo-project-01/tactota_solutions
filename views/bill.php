4519199b0c52b08fefd59f8136768d4fa74cbffe
nuwansasanka <56679037+nuwansasanka@users.noreply.github.com>
2020-11-04 12:59:53 +0530

    add bill

---
+++ views/bill.php
@@ -0,0 +1,196 @@
+<?php
+include 'dash.php';
+
+?>
+
+<!--link rel="stylesheet" href="css/signup.css"-->
+  <link rel="stylesheet" href="css/view_user.css">
+
+  <div class="content">
+ 
+   <div class="main-box">
+    <div class="sub-box">
+
+    <div class="row">
+    <div class="col-75">
+    <b><h2>Bill number-1234</h2></b>
+     </div>
+     <div class="col-25">
+     <b><h2>2020/10/29</h2></b>
+     </div>
+      </div>
+
+    </br>
+        <div class="row">
+    <div class="col-25">
+    <b><label1 for="fname">Product name</label1></b>
+    </div>
+    <div class="col-75">
+    <input type="text" id="pname" name="productname">
+    </div>
+        </div>
+    
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Brand</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="brand" name="brand">
+    </div>
+          </div>
+    
+    
+          <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Model No</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="modelno" name="modelno">
+    </div>
+          </div>
+
+          <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Sales Price</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="salesprice" name="salesprice">
+    </div>
+          </div>
+
+          <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Quantity</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="quantity" name="quantity">
+    </div>
+          </div>
+
+          <div class="row">
+    <div class="col-25">
+    <label for="fname">Discount</label>
+    </div>
+    <div class="col-75">
+    <input type="text" id="discount" name="discount">
+    </div>
+          </div>
+      
+          <div class="row">
+    <div class="col-25">
+    <b><label for="fname">Total Price</label></b>
+    </div>
+    <div class="col-75">
+    <input type="text" id="tprice" name="totalprice">
+    </div>
+          </div>
+
+        </div>
+
+    <div class="sub-box">
+
+        </br></br</br></br></br></br>
+       <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Customer Name</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="cname" name="customername">
+    </div>
+        </div>
+
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Address</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="address" name="firstname">
+    </div>
+        </div>
+
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Telephone No</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="telephoneno" name="telephoneno">
+    </div>
+        </div>
+    
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Email</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="email" name="email">
+    </div>
+        </div>
+
+       </br>
+        <div class="row">
+         <h5 class="left">Payment Method</h5>
+					<input class="text" type="radio" name="payment_method" value="cash" required="">Cash
+					<input class="text" type="radio" name="payment_mehod" value="cheque" required="">Cheque
+        </div>
+    
+        <b><h2>Cheque information</h2></b>
+
+      
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Bank Name</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="bname" name="bankname">
+    </div>
+        </div>
+
+       
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Cheque No</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="chequeno" name="chequeno">
+    </div>
+        </div>
+    
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Due Date</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="duedate" name="due date">
+    </div>
+        </div>
+
+    
+        <div class="row">
+    <div class="col-25">
+    <label1 for="fname">Recived Date</label1>
+    </div>
+    <div class="col-75">
+    <input type="text" id="reciveddate" name="recived date">
+    </div>
+        </div>
+    
+        
+        <div class="row">
+    <div class="col-50">
+    <input type="new-btn" value="Pay">
+    </div>
+    <div class="col-50">
+    <input type="new-btn" value="Cancel">
+    </div>
+        </div>
+    
+
+
+    </div>
+    </div>
+      <div class="footers">
+	  <p>© Tactota Solutions All rights reserved </p>
+</div>
+
+ </div>
+</body>
