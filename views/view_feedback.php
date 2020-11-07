dad651b95a9d1aa7c92010ec0b9e35641bb1b451
nuwansasanka <56679037+nuwansasanka@users.noreply.github.com>
2020-11-07 20:45:22 +0530

    add

---
+++ views/view_feedback.php
@@ -0,0 +1,29 @@
+<?php
+include 'dash.php';
+
+?>
+
+  <link rel="stylesheet" href="bill.css">
+  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="css/view_user.css">
+
+  <div class="content">
+  
+  <!--div class="main-box"-->
+  <div class="row">
+    <div class="col-10">
+  <div class="lbl2"><b>From</b></div>
+    </div>
+    <div class="col-75">
+    <textarea id="email" name="email"></textarea>
+    </div>
+          </div>
+     <br><br><br>
+         <textarea id="email" name="email" height="800px" width="600px"></textarea>
+<!--/div-->
+
+
+
+ 
+    </div>
+</body>
