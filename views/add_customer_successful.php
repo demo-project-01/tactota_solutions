4895ff02b70b1b5d57744525648ca3f006085d25
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-11-12 02:20:15 +0530

    add_customer_successful.php

---
+++ views/add_customer_successful.php
@@ -0,0 +1,33 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tactota Solutions Registration Form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="../public/css/signup.css" rel="stylesheet" type="text/css"/>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+<div>
+    <h1>Successful Adding</h1>
+    <br/>
+    <div class="main-container">
+
+        <div class="sub-container">
+            <form action="#" method="post">
+
+                <p>Customer Details Adding Successfully!</p>
+
+            </form>
+            <br/>
+            <a href="add_customer_details.php" class="next">Back</a>
+
+        </div>
+    </div>
+    <div class="footer">
+        <p>© Tactota Solutions All rights reserved </p>
+    </div>
+</div>
+</body>
+</html>
