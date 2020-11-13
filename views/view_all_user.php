f361b7a16335a34983ad88ca5ffd74100edaf826
Michelle Fernando <54815746+MichelleFdo@users.noreply.github.com>
2020-11-14 02:47:51 +0530

    connect backend to view all users
    this is in clerk dashboard. If it should transfer into admin dashboard, please link the admin sidebar.

---
+++ views/view_all_user.php
@@ -0,0 +1,45 @@
+<?php
+include 'clerk_sidebar.php';
+require '../controller/authenitication.php';
+$data=new authenitication();
+$sql=$data->user_details();
+//session_start();
+//$row=$_SESSION['view_user_details'];
+?>
+<div class="content">
+  <h1 id="tbl-heading"> View Users</h1>
+  
+  <div class="search">
+    <input type="text" placeholder="Search..">
+  </div>
+
+
+    <div class="view-tbl">
+        <table>
+            <thead>
+            <tr>
+                <th>User ID</th>
+                <th scope="col">User Name</th>
+                <th scope="col">Job Position</th>
+                <th scope="col" colspan=3 border=0>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php foreach($sql as $k => $v)
+                    {
+                        ?>
+                <tr>
+                    <td><?php echo $sql[$k]['emp_id']; ?></td>
+                    <td><?php echo $sql[$k]['username']; ?></td>
+                    <td><?php echo $sql[$k]['position']; ?></td>
+                    <td><button class="view">View</button> </td>
+                    <td><button class="update">Update</button> </td>
+                    <td><button class="delete">Delete</button> </td>
+                </tr>
+                    <?php 
+                } ?>
+            </tbody>
+        </table> 
+    </div>
+</div>
+</body>
