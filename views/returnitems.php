9f61545a6f5ce628d8eb1a4a70d7720b30a92978
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-11-14 00:17:47 +0530

    returnitems.php

---
+++ views/returnitems.php
@@ -0,0 +1,15 @@
+<?php
+   include 'clerk_sidebar.php';
+ 
+?><head>
+<link rel="stylesheet" href="../public/css/reminderitems.css">
+</head>
+<div class="content" style="width:auto;">
+    <h1 id="tbl-heading">Return Items</h1><br/>
+    <div class="searchs">
+    <input type="text" placeholder="Search..">
+</div>
+    <div class="footerh">
+			<p>© Tactota Solutions All rights reserved </p>
+      </div>  
+</div>
