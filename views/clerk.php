336a8441d24b5b1c7e9764a2b9e40b155e6652fd
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2020-11-13 17:00:52 +0530

    Connect Backend and modified

--- views/clerk.php
+++ views/clerk.php
@@ -1,74 +1,56 @@
 <?php
    include 'clerk_sidebar.php';
+   require '../controller/inventory_maintain.php';
+   $data=new inventory_maintain();
+   $sql=$data->display_few_reminders();
+   //print_r($sql);
 ?>
 <body>
-<div class="content">
+<div class="content" style="width:auto;">
  <div>
     <div class= "dash1">
-      <b><p class="dash">hello world</p></b>
+      <b><p class="dash">SUPPLIERS</p></b>
     </div>
 
     <div class= "dash2">
-        <b><p class="dash">hello world</p></b>
+        <b><p class="dash">PRODUCTS</p></b>
     </div>
     </div>
 
     <div> 
     <div class= "dash3">
-        <b><p class="dash">hello world</p></b>
+        <b><p class="dash">REMINDERS</p></b>
     </div>
     <div class= "dash4">
-        <b><p class="dash">hello world</p></b>
+        <b><p class="dash">RETURN ITEMS</p></b>
     </div>
     </div>
     
-    <div class="wrapper">
+   <div class="wrapper">
         <div class="list_wrap">
             <div class="subcontent">
-        <h1><p>Stock Reminders</p></h1>
-        </div>
+                <h1><p>Stock <br/>Reminders</p></h1>
+            </div>
             <ul>
+            <?php
+            foreach ($sql as $k => $v){
+                ?>
                 <li class="github">
                     <div class="list">
                         <div class="contentc">
-                            <b>item1</b>
+                            <b><?php echo $sql[$k]["p_name"] ?></b>
                         </div>
+                        
                     </div>
-                </li>
-                <li class="codepen">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>item2</b>
-                        </div>
-                    </div>
-                </li>
-                <li class="dribble">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>item3</b>
-                        </div>
-                    </div>
-                </li>
-                <li class="reddit">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>item4</b>
-                        </div> 
-                    </div>
-                </li>
-                <li class="quora">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>item5</b>
-                        </div> 
-                    </div>
-                </li>
-                <div class="view">
-                <b><a href="#"><span>View All Products</span></a></b>
+                    
+            </li><?php }?>
+                <div class="view"><br/>
+                    <b><a href="reminderitems.php"><span>View All Reminders</span></a></b>
                 </div>
+                
             </ul>
         </div>
-    </div> 
+    </div>
     <div class="footer">
 	<p>© Tactota Solutions All rights reserved </p>
    </div>
