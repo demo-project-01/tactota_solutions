b9fe0cfdae16e1c07715ecde9c7fc2771f79216d
Michelle Fernando <54815746+MichelleFdo@users.noreply.github.com>
2020-11-14 20:12:43 +0530

    Create email.php

---
+++ public/css/email.php
@@ -0,0 +1,65 @@
+.email-tbl
+{
+    display: flex;
+    margin: 2em auto;
+    background:/* #2f323a*/#ffffff;
+    background-size: cover;
+    border:none;
+    width:80%;
+    padding: 2em;
+    align-items: center;
+    text-align: center;
+}
+
+
+.email-tbl th
+{
+    background-color: none;
+
+}
+#email-tr
+{
+    background-color: #ffffff;
+    height: 1em;
+}
+
+.bold
+{
+    font-weight: bolder;
+    font-size: 1em;
+}
+
+
+#email-select
+{
+    margin-right: 20px;
+    width:95%;
+    font-weight: bold;
+    padding:0.8em;
+    border-radius: 5px;
+    border-style: solid;
+    border-width: 1px;
+    background-color:#ffffff;
+}
+
+#email-text
+{       
+    height: 1em;
+    border-radius: 5px;
+    border-style: solid;
+    border-width: 1px;
+    background-color:#ffffff;
+}
+
+#email-textarea
+{
+    margin-right: 20px;
+    width:97%;
+    
+    height: 8em;
+    border-radius: 5px;
+    border-style: solid;
+    border-width: 1px;
+    background-color:#ffffff;
+    resize: none;
+}
