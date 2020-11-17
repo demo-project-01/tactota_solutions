daf9232279622145e0d73daa2cbc8336318ee1b2
MichelleFdo <mufernando02@gmail.com>
2020-11-18 02:23:57 +0530

    register updated icons

--- views/register.php
+++ views/register.php
@@ -7,6 +7,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <link href="../public/css/signup.css" rel="stylesheet" type="text/css"/>
+<script src="https://kit.fontawesome.com/1b83d32a6d.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -20,46 +21,87 @@
 			</div>
 			<div class="sub-container">
 				<form action="../controller/authenitication.php?action=register" method="post">
-					<label for='fname' id='left-label'>First Name</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id='fname' class="text" type="text" name="firstname" required="">
-					<label for='mname' id='left-label'>Middle Name</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id='mname' class="text" type="text" name="middlename">
-					<label for='lname' id='left-label'>Last Name</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id='lname' class="text" type="text" name="lastname" required="">
-					<label for='address' id='left-label'>Home Address</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id="address" class="text" type="text" name="address"required="">
-					<label for='teleno' id='left-label'>Contact Number</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id='teleno' class="text" type="text" name="moblile_no" required="">
-					<label for='nic' id='left-label'>NIC</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input id='nic' class="text" type="text" name="nic" required="">
-					<label for='lname' id='left-label'>DOB</label>
-								<i class="fas fa-lock" class="align"></i>
-								<input class="text" type="text" name="dob" required="">
-					<label for='job' id='left-label'>Job Position </label>
-								<input id='job' class="text" type="radio" name="job_position" value="Clerk" required=""><span style="font-size:0.8em; color:#007042;">Clerk&nbsp&nbsp
-								<input id='job' class="text" type="radio" name="job_position" value="Shop Keeper" required="">Shop Keeper</span>
+
+					<label for='fname' id='left-label'>
+						<i class="fa fa-user" aria-hidden="true"></i></i>
+						&nbsp&nbspFirst Name *
+					</label>
+					<input id='fname' class="text" type="text" name="firstname" required=""> 
+
+					<label for='mname' id='left-label'>
+						<i class="fa fa-user" aria-hidden="true"></i></i>
+						&nbsp&nbspMiddle Name
+					</label>	
+					<input id='mname' class="text" type="text" name="middlename">
+
+					<label for='lname' id='left-label'>
+						<i class="fa fa-user" aria-hidden="true"></i></i>
+						&nbsp&nbspLast Name *
+					</label>
+					<input id='lname' class="text" type="text" name="lastname" required="">
+
+					<label for='address' id='left-label'>
+						<i class="fa fa-address-book" aria-hidden="true"></i>
+						&nbsp&nbspHome Address *
+					</label>
+					<input id="address" class="text" type="text" name="address"required="">
+
+					<label for='teleno' id='left-label'>
+						<i class="fa fa-phone" aria-hidden="true"></i>
+						&nbsp&nbspContact Number *
+					</label>
+					<input id='teleno' class="text" type="text" name="moblile_no" required="">
+
+					<label for='nic' id='left-label'>
+						<i class="fa fa-id-card-o" aria-hidden="true"></i>
+						&nbsp&nbspNIC *
+					</label>
+					<input id='nic' class="text" type="text" name="nic" required="">
+
+					<label for='dob' id='left-label'>
+						<i class="fa fa-calendar" aria-hidden="true"></i>
+						&nbsp&nbspDOB *
+					</label>
+					<input id="dob" class="text" type="text" name="dob" required="">
+
+					<label for='job' id='left-label'>
+						<i class="fa fa-briefcase" aria-hidden="true"></i>
+						&nbsp&nbspJob Position *
+					</label>
+					<input id='job' class="text" type="radio" name="job_position" value="Clerk" required=""><span style="font-size:0.8em; color:#007042;">Clerk&nbsp&nbsp
+					<input id='job' class="text" type="radio" name="job_position" value="Shop Keeper" required="">Shop Keeper</span>
 			</div>
 			<div class="sub-container">
 			
-					<label for='email' id='left-label'>Email Address </label>
-						<i class="fas fa-lock" class="align"></i>
-						<input id='email' class="text email" type="email" name="email" required="">
-					<label for='img' id='left-label'>Image </label>
-						<i class="fas fa-lock" class="align"></i>
-						<input id="img" class="text" type="file" name="nic" >
-					<label for='username' id='left-label'>Username </label>
-						<i class="fas fa-lock" class="align"></i>
-						<input id='username' class="text" type="text" name="Username" required="">
-					<label for='pswd1' id='left-label'>Password </label>
-						<input id='pswd1' class="text" type="password" name="password" required="">
-					<label for='pswd2' id='left-label'>Confirm Password </label>
-						<input id='pswd2' class="text w3lpass" type="password" name="password" required="">
+					<label for='email' id='left-label'>
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+						&nbsp&nbspEmail Address *
+					</label>
+					<input id='email' class="text email" type="email" name="email" required="">
+
+					<label for='img' id='left-label'>
+						<i class="fa fa-file-image-o" aria-hidden="true"></i>
+						&nbsp&nbspImage 
+					</label>
+					<input id="img" class="text" type="file" name="nic" >
+
+					<label for='username' id='left-label'>
+						<i class="fa fa-user" aria-hidden="true"></i>
+						&nbsp&nbspUsername *
+					</label>
+					<input id='username' class="text" type="text" name="Username" required="">
+
+					<label for='pswd1' id='left-label'>
+						<i class="fa fa-key" aria-hidden="true"></i>
+						&nbsp&nbspPassword *
+					</label>
+					<input id='pswd1' class="text" type="password" name="password" required="">
+
+					<label for='pswd2' id='left-label'>
+						<i class="fa fa-key" aria-hidden="true"></i>
+						&nbsp&nbspConfirm Password *
+					</label>
+					<input id='pswd2' class="text w3lpass" type="password" name="password" required="">
 					
 					<div class="wthree-text">
 						<label class="anim">
