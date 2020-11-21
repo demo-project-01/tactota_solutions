5be3d114925e6b948ddce0139c0cb69a0fd638d2
ReshaniD97 <reshanidilhari97@gmail.com>
2020-11-22 01:17:34 +0530

    Update stockreminders.php

--- views/stockreminders.php
+++ views/stockreminders.php
@@ -3,10 +3,13 @@
 $row= $_SESSION['reminderitem_suppliers'];*/
 include 'clerk_sidebar.php';
 require '../controller/inventory_maintain.php';
-session_start();
+//session_start();
 $row= $_SESSION['reminderitem_suppliers'];
+
 ?>
+
 <head>
+<link rel="stylesheet" href="../public/css/update.css">
 <link rel="stylesheet" href="../public/css/reminderitems.css">
 </head>
   <div class="content"style="width:auto;">
@@ -24,6 +27,7 @@ $row= $_SESSION['reminderitem_suppliers'];
             <th>Supplier</th>
             <th scope="col">Address</th>
             <th scope="col">Price</th>
+            <th scope="col">Action</th>
             <th scope="col" colspan=3 border=0></th>
           </tr>
         </thead>
@@ -32,40 +36,22 @@ $row= $_SESSION['reminderitem_suppliers'];
 
 
 ?>
-
-
-
 <tr> 
 
-
+<?php if($row["item_status"]=='1'){?>
     <td><?php echo $row["sup_name"] ?></td>
     <td><?php echo $row["address"] ?></td>
     <td><?php echo $row["p_cost"] ?></td>
-    <td><button class="view">View</button> </td>
-<td><button class="update">Delete</button> </td>
-</tr>
-<?php
+    <td><a href = "email.php"  class="view"><button>View</button> </td>
+    <!--?php
+    foreach ($sql as $k => $v){
 
-?>
-    
-            
-          <!--td><button class="delete">Delete</button> </td-->
-          <!--/tr-->
-  <!-- php //if(count($user)): ?>
-      php //foreach($person as $user): ?>
-      <tr>
-        <td>php// echo $person->emp_id; ?></td>
-        <td>php //echo $person->username; ?></td>
-        <td>php //echo $person->job_position; ?></td>
-        <
-        <td>
-      </tr>
-      php //endforeach; ?>
-      php //else: ?>
-        <tr>
-          <td>No Records Found !</td>
-        </tr>
-      php// endif; ?> -->
+    {?-->
+    <td><a href="../controller/inventory_maintain.php?action=delete_reminder_suppliers&id=<?php echo str_replace('#','%23',$row["serial_no"]); ?>" class="view"><button>Delete</button></a></td>
+    <td><a href = "reminderitems.php"  class="view"><button>Back</button> </td><?php }?>
+  <!--?php  $row["active_status"]=='0' ;?-->
+    <!--td><button class="update">Delete</button> </td-->
+</tr>
       </tbody>
   </table> 
       </div>
@@ -73,4 +59,4 @@ $row= $_SESSION['reminderitem_suppliers'];
 			<p>© Tactota Solutions All rights reserved </p>
       </div>
     </div>
-</body-->
+</body>
