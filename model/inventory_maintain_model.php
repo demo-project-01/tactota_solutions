119e99fd1556d1a7958c3e2062df6450226f4b17
ReshaniD97 <reshanidilhari97@gmail.com>
2020-11-22 01:57:44 +0530

    Update inventory_maintain_model.php

--- model/inventory_maintain_model.php
+++ model/inventory_maintain_model.php
@@ -267,14 +267,8 @@ class inventory_maintain_model
         }
     }
 
-     }
-         public function display_reminder_suppliers($id){          //nuwan
-        $query=$this->mysqli->query("SELECT supplier.sup_name,sup_address.address,product.p_cost FROM supplier_product INNER JOIN supplier ON supplier_product.sup_id=supplier.sup_id INNER JOIN sup_address ON supplier_product.sup_id=sup_address.sup_id INNER JOIN product ON supplier_product.p_id=product.p_id AND product.p_id='" . $id . "'");
-        while ($row = $query->fetch_assoc()) {
-            $result= $row;
-        }
-        return $result;
-    }
+     
+      
     
 
      public function get_delete_product_details($id){
