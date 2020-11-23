7eb4c386142ff1a6760d4ef671eb1c746a1f3a23
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-24 01:18:06 +0530

    update js file

---
+++ public/js/view_all_product.js
@@ -0,0 +1,32 @@
+setTimeout(function() {
+    let alert = document.querySelector(".alert");
+    alert.remove();
+}, 60)
+
+$(document).ready(function(){
+    load_data();
+    function load_data(query)
+    {
+        $.ajax({
+            url:"../controller/inventory_maintain.php?action=update_product",
+            method:"post",
+            data:{query:query},
+            success:function(data)
+            {
+                $('#result').html(data);
+            }
+        });
+    }
+
+    $('#search_text').keyup(function(){
+        var search = $(this).val();
+        if(search != '')
+        {
+            load_data(search);
+        }
+        else
+        {
+            load_data();
+        }
+    });
+});
