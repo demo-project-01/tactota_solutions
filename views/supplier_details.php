46dde74b99cd75c26aae156398c827b3cb376336
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-24 00:06:16 +0530

    update supplier details

--- views/supplier_details.php
+++ views/supplier_details.php
@@ -1,12 +1,10 @@
 <?php
 include 'clerk_sidebar.php';
-require '../controller/inventory_maintain.php';
-$data=new inventory_maintain();
-$sql=$data->view_suppliers();
 ?>
 <head>
 <link rel="stylesheet" href="../public/css/update.css">
-
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+    <script src="../public/js/suplier_details.js"></script>
 </head>
 <div class="content" style="width:auto;">
     <h1 id="tbl-heading">Supplier Details</h1>
@@ -20,41 +18,8 @@ Add new Suppliers</a>
         <input type="text" placeholder="Search..">
     </div>
 
-    <div class="view-tbl">
-        <table>
-            <thead>
-                <tr>
-                   
-                    <th>Supplier Name</th>
-                    <th>Email Address</th>
-                    <th>Address</th>
-                    <th>Contact Number</th>
-                    <th colspan=3>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($sql as $k => $v)
-            {
-                ?>
-                <tr>
-                    
-                    <td><?php echo $sql[$k]["sup_name"] ?></td>
-                    <td><?php echo $sql[$k]["email_address"] ?></td>
-                    <td><?php echo $sql[$k]["address"] ?></td>
-                    <td><?php echo $sql[$k]["telephone_no"] ?></td>
+   <div class="view-tbl" id="result">
 
-                   <td><a href="../controller/inventory_maintain.php?action=supplier_profile&id=<?php
-                      echo $sql[$k]["sup_id"]; ?>" title="view"><i class="fa fa-eye" aria-hidden="true"></i>
-                    </a></td>
-                    <td><a href ="#" title="Update"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
-                    <td><a href ="#" title="Delete"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-                   </tr>
-                <?php
-
-            } ?>
-            </tbody>
-        </table>
     </div>
 </div>
 </body>
