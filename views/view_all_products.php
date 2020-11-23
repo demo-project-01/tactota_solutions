083adb2cea20b4d3bda1cbc3a7ff2745137398cc
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-24 00:19:03 +0530

    update

--- views/view_all_products.php
+++ views/view_all_products.php
@@ -1,58 +1,31 @@
 <?php
 include 'clerk_sidebar.php';
-require '../controller/inventory_maintain.php';
-$data=new inventory_maintain();
-$sql=$data->update_product();
+session_start();
 ?>
 <head>
 <link rel="stylesheet" href="../public/css/update.css">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+    <script src="../public/js/view_all_product.js"></script>    
 </head>
 <div class="content" style="width:auto;">
     <h1 id="tbl-heading">View All Products</h1>
     <div class="search">
-        <input type="text" placeholder="Search..">
+        <input type="text" placeholder="Search.."  id="search_text">
     </div>
     <div>
         <a class="add_button" href="newproduct.php">-|- Add new Product</a>
     </div>
 
- <br>
-    <div class="view-tbl">
-        <table>
-            <thead>
-                <tr>
-                    <th>Product ID</th>
-                    <th>Product Name</th>
-                    <th>Brand Name</th>
-                    <th>Model No</th>
-                    <th>Quantity</th>
-                    <th >Product Cost</th>
-                    <th >Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($sql as $k => $v)
-            {
-                ?>
-                <tr>
-                    <td><?php echo $sql[$k]["p_id"] ?></td>
-                    <td><?php echo $sql[$k]["p_name"] ?></td>
-                    <td><?php echo $sql[$k]["brand_name"] ?></td>
-                    <td><?php echo $sql[$k]["model_no"] ?></td>
-                    <td><?php echo $sql[$k]["quantity"] ?></td>
-                    <td><?php echo $sql[$k]["p_cost"] ?></td>
-                    <td>
-                        <a href="../controller/inventory_maintain.php?action=view_product_details&id=<?php  echo $sql[$k]["p_id"]; ?>" class="view"><button>View</button></a>
-                    </td>
-                    
-                   </tr>
-                <?php
+    <?php if(isset($_SESSION['add_product'])): ?>
+        <div class="alert" id="activate">
+            <span class="activebtn">&times;</span>
+            <strong><?php echo $_SESSION['add_product']; ?></strong>
+        </div>
+    <?php endif; ?>
+    <?php unset($_SESSION['add_product']); ?>
+    <br>
+    <div class="view-tbl" id="result">
 
-            } ?>
-            </tbody>
-        </table>
     </div>
 </div>
-</body>
 
