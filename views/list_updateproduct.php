febecaf022a8dfa7abfb9eb36c07e83f34c83eeb
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-24 00:24:55 +0530

    create list update product

---
+++ views/list_updateproduct.php
@@ -0,0 +1,24 @@
+<?php
+include 'clerk_sidebar.php';
+?>
+<head>
+    <script src="../public/js/list_update.js"></script>
+</head>
+<div class="content" style="width: auto;">
+
+    <h1 id="tbl-heading"> View Product</h1>
+
+    <div class="search">
+        <input type="text" placeholder="Search.." id="search_text">
+    </div>
+    <div class="main-container" id="result">
+
+    </div>
+</div>
+</div>
+</body>
+
+
+
+
+
