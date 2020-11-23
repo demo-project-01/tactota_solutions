cba2ecddeb8007efbff42f0fbb493369b78c0c64
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-23 23:35:15 +0530

    update side bar

--- views/shopkeeper_sidebar.php
+++ views/shopkeeper_sidebar.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tactota solution</title>
     <link rel="stylesheet" href="../public/css/style1.css">
+    <link rel="stylesheet" href="../public/css/view_user.css">
     <link rel="stylesheet" href="../public/css/shopkeeper.css">
-     <link rel="stylesheet" href="../public/css/view_user.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
@@ -30,7 +30,7 @@
 
                 <i class="far fa-user-circle fa-2x"></i>
                 <ul class="menu">
-                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="../controller/authenitication.php?action=profile">Profile</a></li>
                     <li><a href="../controller/authenitication.php?action=logout">Log out</a></li>
                 </ul>
             </div>
@@ -43,8 +43,9 @@
     <a href="shopkeeper_dashbord.php"><i class="fas fa-home"></i></i><span>Dashboard</span></a>
     <a href="#"><i class="fas fa-cart-plus"></i></i><span>Customer Purchase </span></a>
     <a href="purchase.php"><i class="fas fa-money-bill-alt"></i>Purchase</span></a>
-    <a href="#"><i class="fab fa-product-hunt"></i>Return Product</span></a>
+    <a href="returnitems.php"><i class="fab fa-product-hunt"></i>Return Product</span></a>
 
 </div>
+
 
 
