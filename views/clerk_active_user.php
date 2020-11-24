a973ad6f6c21a1eb990f0573ca42a887eb028047
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-24 16:52:27 +0530

    add clerk_active_users and add clerk_users_result

---
+++ views/clerk_active_user.php
@@ -0,0 +1,20 @@
+<?php
+include 'clerk_sidebar.php';
+
+?>
+ <head>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="../public/js/users.js"></script>
+ </head>
+<div class="content" style="width: auto;">
+
+    <h1 id="tbl-heading" style="text-align: left;font-size=20px;font-weight: bold;"> Users</h1>
+
+    <div class="search">
+        <input type="text" id="search_text" placeholder="Search..">
+    </div>
+
+    <div class="view-tbl" id="result">
+
+    </div>
+</div>
---
+++ views/clerk_users_result.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+$sql=$_SESSION['active_user'];
+?>
+
+<table>
+    <thead>
+    <tr>
+
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Job Position</th>
+
+        <th scope="col">View Details</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+
+    foreach ($sql as $k => $v){
+    ?>
+
+
+    <tr>
+
+        <td><?php echo $sql[$k]["first_name"] ?></td>
+        <td><?php echo $sql[$k]["last_name"] ?></td>
+        <td><?php echo $sql[$k]["position"] ?></td>
+
+
+
+
+
+        <td><a href="../controller/authenitication.php?action=view_profile&id=<?php  echo $sql[$k]["emp_id"]; ?>" class="view"><button>View</button></a></td>
+
+        <?php
+
+        } ?>
+    </tbody>
+</table>
