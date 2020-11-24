c9a5161be367d17c5ff44dcc7b2794556f8b4923
ReshaniD97 <reshanidilhari97@gmail.com>
2020-11-24 21:10:12 +0530

    Update review.php

--- views/review.php
+++ views/review.php
@@ -6,42 +6,45 @@
    //print_r($sql);
 ?>
 <link rel="stylesheet" href="..public/css/style1.css">
+<link rel="stylesheet" href="../public/css/update.css">
+<link rel="stylesheet" href="../public/css/report.css">
 <div class="content">
 
-<div class="btn-group">
-  <button class="button1">Week</button>
-  <button class="button1">Month</button>
-  <button class="button1">Year</button>
-</div>
 
-    <b><h1 id="tbl-heading">Customer Review</h1></b>
+
+<b><h1 id="tbl-heading">Customer Review</h1></b>
+<br/>
+<br/>
+<ul>
+<li><a class="button" href="#">Annual Report</a></li>
+    <li><a class="button" href="#">Monthly Report</a></li>
+    <li><a class="button" href="#">Weekly Report</a></li>
+</ul>
    
 <!--div class="main-container" id="view-tbl">
         <div class="search">
             <input type="text" placeholder="Search..">
         </div-->
     <!--div class="main-container" id="view-tbl"-->
-    <div class="view-tbl">
+    <div class="update-tbl">
        <table>
             <thead>
                 <tr>
                     <th>Email</th>
                     
-                    <th scope="col" colspan=2 border=0></th>
+                    <th scope="col" colspan=2 border=0>Action</th>
                 </tr>
             </thead>
             <tbody>
            
                     <td>nuwansasanka1@gmail.com</td>
             
-                    <td><a href><button>View</button></a></td>
-                    <td><a href><button>Delete</button></a></td>
+                    <td><a href="#" title="View"><i class="fa fa-eye" aria-hidden="true">&nbsp&nbspView</i></a></td>
+                    <td><a href="#" title="Delete"><i class="fa fa-trash-o" aria-hidden="true">&nbsp&nbspDelete</i></a></td>
         
 
             </tbody>
        </table>
     </div>
-    <div class="footers">
-			<p>© Tactota Solutions All rights reserved </p>
-      </div>  
+      
 </div>
