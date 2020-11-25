55674a5b9af15217a51457705934b7821101ce44
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-11-25 16:47:15 +0530

    shopkeeper dashboard search result

---
+++ views/search_product_result.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$sql=$_SESSION['dashbord_search'];
+ // print_r($rows);
+?>
+<head>
+    <link rel="stylesheet" href="../public/css/search.css">
+</head>
+
+
+
+    <?php
+    foreach ($sql as $k => $v){
+        ?>
+      <ul id="myUL">
+        <li><a href="../controller/sales.php?action=view_search_product&id=<?php  echo $sql[$k]['p_name']; ?>&id1=<?php  echo $sql[$k]['model_no']; ?>"> <?php  echo $sql[$k]['p_name']   ?> <?php  echo $sql[$k]['model_no']   ?> </a></li>
+
+      </ul>
+
+        <?php
+    } ?>
+
