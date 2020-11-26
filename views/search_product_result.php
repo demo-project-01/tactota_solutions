d34590f8057dadf97861fcf127d6184d42e2aa6e
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2020-11-27 03:48:15 +0530

    update shopkeeper dashbord

--- views/search_product_result.php
+++ views/search_product_result.php
@@ -13,7 +13,7 @@ $sql=$_SESSION['dashbord_search'];
     foreach ($sql as $k => $v){
         ?>
       <ul id="myUL">
-        <li><a href="../controller/sales.php?action=view_search_product&id=<?php  echo $sql[$k]['p_name']; ?>&id1=<?php  echo $sql[$k]['model_no']; ?>"> <?php  echo $sql[$k]['p_name']   ?> <?php  echo $sql[$k]['model_no']   ?> </a></li>
+        <li><a href="../controller/sales.php?action=view_search_product&id=<?php  echo $sql[$k]['p_name']; ?>&id1=<?php  echo $sql[$k]['model_no']; ?>"> <?php  echo $sql[$k]['p_name']?> <?php  echo $sql[$k]['model_no']   ?> </a></li>
 
       </ul>
 
