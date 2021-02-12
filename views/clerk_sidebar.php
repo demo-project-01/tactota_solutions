be0b51ca55a3d558d91ee4aa6f694ddb28907f2d
Reshani Dilhari <56593064+ReshaniD97@users.noreply.github.com>
2021-02-13 00:46:03 +0530

    update clerk_sidebar

--- views/clerk_sidebar.php
+++ views/clerk_sidebar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Template</title>
+    <title>Clerk</title>
     <link rel="stylesheet" href="../public/css/style1.css">
     <link rel="stylesheet" href="../public/css/clerk.css">
     <link rel="stylesheet" href="../public/css/view_user.css">
