21d050b408ccb6fd4b843bda8de046067949eda5
Reshani Dilhari <56593064+ReshaniD97@users.noreply.github.com>
2021-02-13 00:45:47 +0530

    modifiy inventory model

--- views/shopkeeper_sidebar.php
+++ views/shopkeeper_sidebar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tactota solution</title>
+    <title>Shopkeeper</title>
     <link rel="stylesheet" href="../public/css/style1.css">
     <link rel="stylesheet" href="../public/css/view_user.css">
     <link rel="stylesheet" href="../public/css/shopkeeper.css">
