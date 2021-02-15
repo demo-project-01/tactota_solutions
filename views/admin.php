683d7c5547c4022881bf93e8f7d021f4ad8b03ca
MichelleFdo <mufernando02@gmail.com>
2021-02-15 05:54:39 +0530

    boru chart ekak dala baluwa

--- views/admin.php
+++ views/admin.php
@@ -1,6 +1,34 @@
 <?php
    include 'admin_sidebar.php';
 ?>
+<head>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+    </head>
 <body>
 <div class="content" style="width:auto;">             
     <div>
@@ -36,6 +64,7 @@
     <div class="row">
         <div class="graph">
             <b><p class="incomes">SALES CHART OF THE MONTH</p></b>
+            <div id="columnchart_material" style="height: 350px;"></div>
         </div>
         <div class="column">
             <div>
