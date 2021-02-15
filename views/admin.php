984c4766441e19f1972e45d5ee545b7fd2bf5238
MichelleFdo <mufernando02@gmail.com>
2021-02-15 05:31:42 +0530

    update manager dashboard with new features

--- views/admin.php
+++ views/admin.php
@@ -4,12 +4,11 @@
 <body>
 <div class="content" style="width:auto;">             
     <div>
-    <a href ="users.php">
-       <div class="income1">
+        <a href ="users.php">
+        <div class="income1">
             <i class="fas fa-users fa-3x icon-left"></i>
             <b><p class="incomes">USERS</p></b>
             <b><p class="inform">10 Record(s)</p></b>
-            
         </div>
         </a>
         <a href ="review.php">
@@ -34,98 +33,28 @@
         </div>
         </a>   
     </div>
-    <div class="wrapper">
-        <div class="list_wrap">
-        <div class="contentch"><h1><p>Cheque Reminders</p></h1></div></br>
-        <div class="admin-tbl">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Cheque ID</th>
-                        <th>Bank Name</th>
-                        <th>Amount </th>
-                        <th>Due Date</th>
-                        
-                    </tr>
-                </thead>
-                <tboady>
-                    <tr>
-                        <td>CHE001</td>
-                        <td>BOC</td>
-                        <td>20,000/=</td>
-                        <td>2020-11-10</td>
-                    </tr>
-                    <tr>
-                        <td>CHE002</td>
-                        <td>BOC</td>
-                        <td>20,000/=</td>
-                        <td>2020-11-15</td>
-                    </tr>
-                    <tr>
-                        <td>CHE003</td>
-                        <td>NSB</td>
-                        <td>50,000/=</td>
-                        <td>2020-11-12</td>
-                    </tr>
-                    <tr>
-                        <td>CHE004</td>
-                        <td>HNB</td>
-                        <td>20,000/=</td>
-                        <td>2020-12-10</td>
-                    </tr>
-                    <tr>
-                        <td>CHE005</td>
-                        <td>NDB</td>
-                        <td>60,000/=</td>
-                        <td>2020-10-10</td>
-                    </tr>
-                    <tr>
-                        <td colspan=4 style="text-align:center;"><b><a href="view_all_cheques.php"class="view">View All Cheque</a></b></td>
-                    </tr>
-                </tboady>
-            </table>
-            </div>
-            <!--ul>
-                <li class="github">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>Bill1</b>
-                        </div>
-                    </div>
-                </li>
-                <li class="codepen">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>Bill2</b>
-                        </div>
-                    </div>
-                </li>
-                <li class="dribble">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>Bill3</b>
-                        </div>
-                    </div>
-                </li>
-                <li class="reddit">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>Bill4</b>
-                        </div> 
-                    </div>
-                </li>
-                <li class="quora">
-                    <div class="list">
-                        <div class="contentc">
-                            <b>Bill5</b>
-                        </div> 
-                    </div>
-                </li><br/></ul-->
-                
-            
+    <div class="row">
+        <div class="graph">
+            <b><p class="incomes">SALES CHART OF THE MONTH</p></b>
         </div>
-
+        <div class="column">
+            <div>
+                <p class="incomes">TOP SELLING ITEM OF THE MONTH</p>
+                <img src="../public/images/topselling.png">
+            </div>
+            <div>
+                <p class="incomes">LEAST SELLING ITEM OF THE MONTH</p>
+            </div>
+        </div>
+        <div class="card emp">
+            <img src="../public/images/bestsales.png" alt="" width="100px" height="auto"><br/>
+            <p class="incomes">BEST PERFORMING SALES PERSON OF THE MONTH</p>
+            <br/>
+            <p style="font-size:20px;color:#6D6E31;"><b>Mr.Amal Perera</b></p>
+            <p style="color:red;font-size:40px;margin-top:12px;"><b>20</b></p>Sales</p>
+        </div>
     </div>
+    
     <div class="footer">
 	<p>© Tactota Solutions All rights reserved </p>
 </div>
