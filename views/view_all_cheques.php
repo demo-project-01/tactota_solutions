47b7e47a7bb35e570050a5016044d6115f5538f7
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-03-24 22:54:04 +0530

    update search all cheques details

--- views/view_all_cheques.php
+++ views/view_all_cheques.php
@@ -1,9 +1,9 @@
 <?php
    include 'admin_sidebar.php';
-   require '../controller/sales.php';
-   $data=new sales();
-   $sql=$data->view_cheques();
-   if(isset($_GET['action'])){
+  // include 'view_all_cheques_search.php';
+  // $data=new sales();
+   //$sql=$data->view_cheques();
+ /*  if(isset($_GET['action'])){
       if($_GET["action"]=="bank"){
           $sql=$data->view_cheques_by_bank();
       }
@@ -11,16 +11,18 @@
           $sql=$data->view_cheques();
           
       }
-     }
+     }*/
 ?>
 <head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 <link rel="stylesheet" href="../public/css/view_user.css">
+<script src="../public/js/view_all_cheques.js"></script>  
 </head>
 <body>
-   <div class="content" style="width:auto;">
+<div class="content" style="width:auto;">
       <h1 id="tbl-heading">All Cheques</h1><br/>
       <div class="new">
-    <a class="add_button" href="view_all_cheques.php?action=bank">
+      <a class="add_button" href="view_all_cheques.php?action=bank">
         <i class="fa fa-bank" aria-hidden="true"></i>
         &nbsp&nbspBank</a>
     <a class="add_button" href="view_all_cheques.php?action=due_date">
@@ -28,40 +30,10 @@
         &nbsp&nbspDue Date</a>
     </div>
       <div class="search">
-    <input type="text" placeholder="Search..">
+    <input type="text" placeholder="Search.." id="search_text">
     </div>
-    <div class="view-tbl">
-       <table>
-         <thead>
-            <tr>
-               <th>Cheque Number</th>
-               <th>Bill Number</th>
-               <th>Bank Name</th>
-               <th>Amount </th>
-               <th>Due Date</th>
-               <th>Action</th>             
-            </tr>
-         </thead>
-         <tboady>
-         <?php
-
-foreach ($sql as $k => $v)
-{
-    ?>
-    <tr>
-        
-        <td><?php echo $sql[$k]["cheque_id"] ?></td>
-        <td><?php echo $sql[$k]["bill_no"] ?></td>
-        <td><?php echo $sql[$k]["bank_name"] ?></td>
-        <td><?php echo $sql[$k]["amount"] ?></td>
-        <td><?php echo $sql[$k]["due_date"] ?></td>
-        <td><a href="../controller/sales.php?action=cheque_clearance&id=<?php  echo $sql[$k]["cheque_id"]; ?>" title="view">
-         <i class="fa fa-eye" aria-hidden="true" id="tbl-icon">&nbsp&nbspView</i></a>
-    </tr>
-    <?php
-} ?>
-         </tbody>
-       </table>
+    <div class="view-tbl" id="result">
+     
     </div>
     <div class="footer">
 	<p>© Tactota Solutions All rights reserved </p>
---
+++ views/view_all_cheques_search.php
@@ -0,0 +1,51 @@
+<?php
+
+require '../controller/sales.php';
+//session_start();
+$sql=$_SESSION['search_cheque'];
+//print_r($sql);
+$data=new sales();
+ if(isset($_POST['action'])){
+    if($_POST["action"]=="bank"){
+        $sql=$data->view_cheques_by_bank();
+    }
+    else if($_POST["action"]=="customer_return"){
+        $sql=$data->view_cheques();
+        
+    }
+   }
+
+?>
+ <table>
+         <thead>
+            <tr>
+               <th>Cheque Number</th>
+               <th>Bill Number</th>
+               <th>Bank Name</th>
+               <th>Amount </th>
+               <th>Due Date</th>
+               <th>Action</th>             
+            </tr>
+         </thead>
+         <tboady>
+         <?php
+
+foreach ($sql as $k => $v)
+{
+    ?>
+    <tr>
+        
+        <td><?php echo $sql[$k]["cheque_id"] ?></td>
+        <td><?php echo $sql[$k]["bill_no"] ?></td>
+        <td><?php echo $sql[$k]["bank_name"] ?></td>
+        <td><?php echo $sql[$k]["amount"] ?></td>
+        <td><?php echo $sql[$k]["due_date"] ?></td>
+        <td><a href="../controller/sales.php?action=cheque_clearance&id=<?php  echo $sql[$k]["cheque_id"]; ?>" title="view">
+         <i class="fa fa-eye" aria-hidden="true" id="tbl-icon">&nbsp&nbspView</i></a>
+    </tr>
+    <?php
+} ?>
+         </tbody>
+       </table>
+
+
